036006e9455455e17e0d42ecb89274a48be45efc
akash101997 <akash.singh@webninjaz.com>
2025-11-18 22:00:31 +0530

    Object learning Continues

--- object/Calculator.java
+++ object/Calculator.java
@@ -1,11 +1,11 @@
-package object;
+ package object;
 
  class DemoCalculator{
     int a;
-    public int add()
+    public int add(int n1, int n2)
      {
-        System.out.println("In add");
-        return 0;
+        int result = n1 + n2;
+        return result;
     }
 
 }
@@ -15,7 +15,8 @@ public class Calculator {
         int num1 = 4;
         int num2 = 6;
         DemoCalculator calc = new DemoCalculator(); 
-         
-        calc.add();
+         int result = calc.add(num1, num2);
+        // calc.add();
+        System.out.println(result);
     }
 }
